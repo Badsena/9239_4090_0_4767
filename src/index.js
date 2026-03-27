6b3a5156c83186a2f68ccdab7ed093a01f34fce7
Badsena <sena@amypo.in>
2026-03-27 12:07:38 +0000

    User commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
-import Todo from "./Todo";
+import Todo from "./Todos";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
